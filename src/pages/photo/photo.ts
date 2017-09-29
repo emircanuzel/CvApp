409e6b74a91e10f6761f34e33470144031bcc1d9
Emircan <emircanuzel95@gmail.com>
2017-09-29 14:02:42 +0300

    Kamera

--- src/pages/photo/photo.ts
+++ src/pages/photo/photo.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, AlertController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 /**
  * Generated class for the PhotoPage page.
@@ -15,7 +16,12 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class PhotoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,private view:ViewController) {
+public parameter1 = '';
+  public parameter2 = '';
+  public photos: any;
+  public base64Image: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,private view:ViewController,private alertCtrl: AlertController, private camera: Camera) {
   }
    closeModal(){
 
@@ -24,6 +30,54 @@ export class PhotoPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PhotoPage');
+  }
+
+
+ ngOnInit() {
+    this.photos = [];
+
+  }
+
+ takePhoto() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE,
+      cameraDirection: 1,
+      correctOrientation: false
+    }
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      this.base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.photos.push(this.base64Image);
+      this.photos.reverse();
+    }, (err) => {
+      // Handle error
+    });
+  }
+
+  deletePhoto(index) {
+    let confirm = this.alertCtrl.create({
+      title: 'Fotoğrafı silmek istedğinden emin misin?',
+      message: '',
+      buttons: [
+        {
+          text: 'Hayır',
+          handler: () => {
+          }
+        },
+        {
+          text: 'Evet',
+          handler: () => {
+            this.photos.splice(index, 1);
+          }
+        }
+      ]
+    });
+    confirm.present();
+
   }
 
 }
